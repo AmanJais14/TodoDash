0a02f5b6d5a2adf7dacdafd03fa6ee4ae0678186
Rishabhjaiswal2006 <ajamanjaiz14@gmail.com>
2024-08-08 14:30:45 +0530

    added edit and local storage feature

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,18 +1,43 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
+// import { FaEdit } from "react-icons/fa";
 
 function Todo() {
   let [todos, setTodos] = useState([]);
   let [todo, setTodo] = useState("");
+  let [editingId, setEditingId] = useState(null);
+
+  useEffect(() => {
+    const savedTodos = JSON.parse(window.localStorage.getItem("todos"));
+    console.log("Loaded todos from local storage:", savedTodos);
+    if (savedTodos !== null) setTodos(savedTodos);
+  }, []);
+
+  // Save todos to local storage whenever the todos state changes
+  useEffect(() => {
+    console.log("Saving todos to local storage:", todos);
+    window.localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   let handleChange = function (e) {
     setTodo(e.target.value);
   };
   let handleAdd = function () {
-    console.log("added");
-    if (todo.length == 0) return;
-    setTodos([...todos, { todo: todo, id: uuidv4(), isDone: false }]);
+    // console.log("added");
+    if (todo.length === 0) return;
+
+    if (editingId) {
+      setTodos(
+        todos.map((task) =>
+          task.id === editingId ? { ...task, todo: todo } : task
+        )
+      );
+      setEditingId(null);
+    } else {
+      setTodos([...todos, { todo: todo, id: uuidv4(), isDone: false }]);
+    }
+
     setTodo("");
   };
 
@@ -22,10 +47,16 @@ function Todo() {
   let handleDone = function (id) {
     setTodos(
       todos.map((task) => {
-        return task.id === id ? { ...task, isDone: !task.isDone } : task;
+        return (task.id === id ? { ...task, isDone: !task.isDone } : task);
       })
+       
     );
   };
+
+  let handleEdit = function(task){
+      setTodo(task.todo);
+      setEditingId(task.id);
+  }
 
   return (
     <div className="p-10  max-w-4xl m-auto">
@@ -45,21 +76,35 @@ function Todo() {
             className="bg-blue-500 m-2 p-2 rounded-md text-sm hover:cursor-pointer text-zinc-100"
             onClick={() => handleAdd()}
           >
-            Add Task
+           {editingId ? "Update Task" : "Add Task"}
           </div>
         </div>
         <div>
           {todos.map((task, index) => (
             <div className="flex items-center justify-between m-1 " key={index}>
               <div className="flex">
-                <input
-                  type="checkbox"
-                  name=""
-                  onClick={() => {
-                    handleDone(task.id);
-                  }}
-                  id=""
-                />
+                {!task.isDone && (
+                  <input
+                    type="checkbox"
+                    name=""
+                    onClick={() => {
+                      handleDone(task.id);
+                    }}
+                    id=""
+                  />
+                )}
+                {task.isDone && (
+                  <input
+                    type="checkbox"
+                    checked={true}
+                    name=""
+                    onClick={() => {
+                      handleDone(task.id);
+                    }}
+                    id=""
+                  />
+                )}
+
                 <div
                   className={`text-zinc-900 p-2 ${
                     task.isDone ? "line-through" : ""
@@ -68,14 +113,18 @@ function Todo() {
                   {task.todo}
                 </div>
               </div>
+              <div className="flex gap-2 items-center"> 
+
+              <div className="text-xs bg-green-400 p-1 rounded-md cursor-pointer" onClick={()=>{handleEdit(task)}}>Edit</div>
               <div
                 className="bg-red-500 text-zinc-100 text-xs p-2 rounded-lg cursor-pointer"
                 onClick={() => {
                   handleDelete(task.id);
                 }}
-              >
+                >
                 Delete
               </div>
+                </div>
             </div>
           ))}
         </div>
